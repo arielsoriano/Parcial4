44452abb7843131a637c0aea0566a5f1fd94eaf5
Giuliano Ortego <48778667+GiulianoOrtego@users.noreply.github.com>
2019-11-09 16:21:21 -0300

    Add files via upload

---
+++ Parcial4/src/objetos/Turno.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package objetos;
+
+/**
+ *
+ * @author ariel
+ */
+public class Turno {
+    
+    int id;
+    String codigo;
+    String descripcion;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    
+}
