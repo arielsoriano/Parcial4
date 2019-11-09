720be3b6c41d28201839459989525ebacddff039
Pame Peralta <57571456+pamelincon25@users.noreply.github.com>
2019-11-09 15:41:16 -0300

    Add files via upload

---
+++ Parcial4/src/objetos/Grado.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package objetos;
+
+/**
+ *
+ * @author ariel
+ */
+public class Grado {
+    
+    int id;
+    int numero;
+    String seccion;
+    Turno turno;
+    Alumno alumno;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getSeccion() {
+        return seccion;
+    }
+
+    public void setSeccion(String seccion) {
+        this.seccion = seccion;
+    }
+
+    public Turno getTurno() {
+        return turno;
+    }
+
+    public void setTurno(Turno turno) {
+        this.turno = turno;
+    }
+
+    public Alumno getAlumno() {
+        return alumno;
+    }
+
+    public void setAlumno(Alumno alumno) {
+        this.alumno = alumno;
+    }
+    
+}
