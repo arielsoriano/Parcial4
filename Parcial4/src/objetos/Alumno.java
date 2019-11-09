66514462361d202a70a72416653e1c82c14cfddc
arielsoriano <50920501+arielsoriano@users.noreply.github.com>
2019-11-09 15:14:16 -0300

    Add files via upload

---
+++ Parcial4/src/objetos/Alumno.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package objetos;
+
+/**
+ *
+ * @author ariel
+ */
+public class Alumno extends Persona{
+    
+    int id;
+    int matricula;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+    
+    
+}
