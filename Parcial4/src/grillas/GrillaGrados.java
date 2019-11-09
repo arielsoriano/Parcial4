f61a9cbde00c8b66378d34172d869cad903410b8
Pame Peralta <57571456+pamelincon25@users.noreply.github.com>
2019-11-09 17:56:35 -0300

    Add files via upload

---
+++ Parcial4/src/grillas/GrillaGrados.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package grillas;
+
+import objetos.Grado;
+import java.util.ArrayList;
+import javax.swing.table.AbstractTableModel;
+
+/**
+ *
+ * @author pamel
+ */
+public class GrillaGrados extends AbstractTableModel{
+
+    private ArrayList<Grado> grados = new ArrayList<>();
+    
+    public GrillaGrados(ArrayList grados) {
+        this.grados = grados;
+    }
+
+    public GrillaGrados() {
+    }
+    
+
+    @Override
+    public int getRowCount() {
+        return grados.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return 2;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        Grado grado = (Grado) grados.get(rowIndex);
+        switch(columnIndex){
+                case 0: return grado.getNumero();
+                case 1: return grado.getSeccion();
+                default: return "";
+                }
+           
+        }
+            
+        
+    @Override
+    public String getColumnName(int column) {
+         switch(column){
+            case 0: return "Numero";
+            case 1: return "Seccion";
+            default: return "";
+            }
+    }
+    
+    public void removerRegistro(int registro) {
+        grados.remove(registro);
+        this.fireTableRowsDeleted(registro, registro);
+}
+}
