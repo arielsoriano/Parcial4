08831ae557af0f7ed62d105c19b810473b310602
Agustina <agus-cs-10@hotmail.com>
2019-11-09 17:23:17 -0300

    objeto cargo

---
+++ Parcial4/src/objetos/Cargo.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package objetos;
+
+/**
+ *
+ * @author Agustina
+ */
+public class Cargo {
+    
+    int id;
+    int codigo;
+    String descripcion;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    
+    
+}
